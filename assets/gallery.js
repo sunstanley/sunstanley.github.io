764dcb46bfa4853221e53fe10c5444ef3e079813
sunstanley <shsun@stanley-suns-macbook-pro-3.local>
2018-07-02 04:27:33 -0400

    Update main gallery
    update font-awesome, update featherlight gallery, layered prev & next buttons

--- assets/gallery.js
+++ assets/gallery.js
@@ -5,9 +5,17 @@ $(document).ready(function () {
         galleryFadeIn: 1000,
         galleryFadeOut: 300,
         closeOnClick: 'background',
-        previousIcon: '<span class="fa fa-chevron-circle-left" aria-hidden="false"></span>',
-        nextIcon: '<span class="fa fa-chevron-circle-right" aria-hidden="true"></span>',
-        closeIcon: '<span class="fa fa-times-circle" aria-hidden="true"></span>',
-        variant: 'f-gallery'
+        previousIcon: '<span class="fa-layers fa-fw"><i class="fas fa-circle" data-fa-transform="down-11 shrink-4" style="color:darkOrange"></i><i class="fa-inverse fas fa-chevron-circle-left" data-fa-transform="down-11 shrink-3"></i></span>',
+        nextIcon: '<span class="fa-layers fa-fw"><i class="fas fa-circle" data-fa-transform="down-11 shrink-4" style="color:darkOrange"></i><i class="fa-inverse fas fa-chevron-circle-right" data-fa-transform="down-11 shrink-3"></i></span>',
+        closeIcon: '<span class="fas fa-times-circle"></span>',
+        variant: 'f-gallery',
+        afterContent: function(event) {
+            // make sure new image is actually hidden before it fades in
+            $(this.$content).fadeTo(0, 0);
+        },
+        afterOpen: function(event) {
+            // this function is necessary to prevent first image from being hidden
+            $(this.$content).fadeTo(this.galleryFadeIn, 1);
+        },
     });
 });
